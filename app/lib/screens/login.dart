c2acf8a60d5f19dc044932203c7ca667dd1f9ebc
Vedant Bahel <vedant556@gmail.com>
2022-06-02 16:18:38 +0530

    login_screen

--- app/lib/screens/login.dart
+++ app/lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class login_page extends StatelessWidget {
   @override
@@ -17,7 +18,7 @@ class login_page extends StatelessWidget {
             child: Center(
               child: Column(
                 children: [
-                  Text("\n \n \n \n"),
+                  Text("\n \n"),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Image(
@@ -27,44 +28,169 @@ class login_page extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(30.0),
+                    padding: const EdgeInsets.all(40.0),
                     child: Text("Welcome to MLDLS",
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            color: Colors.deepOrangeAccent,
-                            fontSize: 25)),
+                            color: Color.fromARGB(255, 255, 128, 54),
+                            fontSize: 30)),
                   ),
-                  Text("Login with email"),
+                  Text("Login with email", style: TextStyle(fontSize: 15)),
                   Padding(
-                    padding: const EdgeInsets.all(40.0),
+                    padding: const EdgeInsets.all(30.0),
                     child: Container(
                       child: Column(
                         children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                                fillColor: Colors.white.withOpacity(1),
-                                filled: true,
-                                labelText: 'Email',
-                                labelStyle: TextStyle(
-                                  color: Colors.black,
-                                ),
-                                hintText: "abc@xyz.com"),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              width: 400.0,
+                              height: 60.0,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            color: Colors.white, width: 2.0),
+                                        borderRadius:
+                                            BorderRadius.circular(20.0)),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    fillColor: Colors.white.withOpacity(0.8),
+                                    filled: true,
+                                    labelText: 'Email',
+                                    labelStyle: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                    hintText: "abc@xyz.com"),
+                              ),
+                            ),
                           ),
-                          Text("\n"),
-                          TextFormField(
-                            decoration: InputDecoration(
-                                fillColor: Colors.white.withOpacity(0.6),
-                                filled: true,
-                                labelText: 'Email',
-                                labelStyle: TextStyle(
-                                  color: Colors.black,
-                                ),
-                                hintText: "**********"),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              width: 400.0,
+                              height: 60.0,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            color: Colors.white, width: 2.0),
+                                        borderRadius:
+                                            BorderRadius.circular(20.0)),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    fillColor: Colors.white.withOpacity(0.8),
+                                    filled: true,
+                                    labelText: 'Password',
+                                    labelStyle: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                    hintText: "**********"),
+                              ),
+                            ),
                           ),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text("Login"),
-                          )
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: SizedBox(
+                              width: 400.0,
+                              height: 60.0,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(fontSize: 22),
+                                ),
+                                style: ButtonStyle(
+                                    foregroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Colors.white),
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Color.fromARGB(255, 255, 128, 54)),
+                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(18.0),
+                                            side: BorderSide(
+                                                color: Color.fromARGB(
+                                                    255, 255, 128, 54))))),
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Forgot Password?",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color.fromARGB(255, 87, 87, 87)),
+                              )),
+                          Row(children: <Widget>[
+                            Expanded(
+                              child: new Container(
+                                  margin: const EdgeInsets.only(
+                                      left: 10.0, right: 20.0),
+                                  child: Divider(
+                                    color: Colors.black,
+                                    height: 36,
+                                  )),
+                            ),
+                            Text("or"),
+                            Expanded(
+                              child: new Container(
+                                  margin: const EdgeInsets.only(
+                                      left: 20.0, right: 10.0),
+                                  child: Divider(
+                                    color: Colors.black,
+                                    height: 36,
+                                  )),
+                            ),
+                          ]),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: SizedBox(
+                              width: 400.0,
+                              height: 60.0,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset("images/google.png",
+                                        width: 30,
+                                        height: 30,
+                                        fit: BoxFit.fill),
+                                    Text("\t \t \t Log In with Google",
+                                        style: TextStyle(fontSize: 20)),
+                                  ],
+                                ),
+                                style: ButtonStyle(
+                                    foregroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Colors.black),
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Colors.white),
+                                    shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(18.0),
+                                            side: BorderSide(
+                                                color: Colors.white)))),
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "New User? Sign up",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color.fromARGB(255, 87, 87, 87)),
+                              )),
                         ],
                       ),
                     ),
